b2bd2526ca6e860c9850fc5679bfc79cd8d4ee08
jurgengjoncari <j.gjoncari@albansi.com>
2024-12-29 23:35:48 +0100

    add categories

---
+++ server/updates/01/00/0000.js
@@ -0,0 +1,18 @@
+/**
+ * add categories
+ */
+const app = require('../../../server');
+const {Category} = app.models;
+
+async function _0000() {
+  try {
+    const categories = require('../../../data/Category.json');
+
+    await Category.create(categories);
+    console.log('Categories added successfully!');
+  } catch (err) {
+    console.error('Error adding categories:', err);
+  }
+}
+
+_0000();
